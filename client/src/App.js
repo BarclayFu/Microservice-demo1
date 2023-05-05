f8323f89a697a3611ef1ff96d2e84a7df5de1a33
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-05-05 08:33:22 -0400

    update post submission

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostCreate from './PostCreate';
+
+
+export default() => {
+    return <div>
+        <h1>Create Posts</h1>
+        <PostCreate/>
+    </div>;
+
+};
