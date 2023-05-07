66750b0c3d38631eac89dac5c87b64309bf41b2d
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-05-07 19:07:30 -0400

    update cors

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -3,9 +3,18 @@ import axios from 'axios';
 
 export default()=>{
     const[title, setTitle] = useState('');
+    const onSubmit = async (event) =>{
+        event.preventDefault();
+
+        await axios.post('http://localhost:4000/posts',{
+            title
+        });
+        setTitle('');
+    };
+
     return (
         <div>
-            <form onSubmit={ onSubmit}>
+            <form onSubmit={onSubmit}>
                 <div className = "form-group">
                     <label>Title</label>
                     <input value={title} onChange={e=> setTitle(e.target.value)} className="form-control"/>
