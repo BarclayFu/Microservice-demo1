63168debcdb5808f7c55f11d126bf5d4f45fee12
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-05-06 22:37:56 -0400

    Update PostCreate.js

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from 'axios';
 
 export default()=>{
-    return <div>
-        <form>
-            <div className = "form-group">
-                <label>Title</label>
-                <input className="form-control"/>
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
-    </div>;
+    const[title, setTitle] = useState('');
+    return (
+        <div>
+            <form onSubmit={ onSubmit}>
+                <div className = "form-group">
+                    <label>Title</label>
+                    <input value={title} onChange={e=> setTitle(e.target.value)} className="form-control"/>
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        );
 };
