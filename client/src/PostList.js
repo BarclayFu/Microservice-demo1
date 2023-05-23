574992c6d2f70c150e1a8ec6aca670db92bd6d41
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-05-22 23:25:59 -0400

    update PostList

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export default() => {
     const [posts, setPosts] = useState({});
@@ -13,5 +13,22 @@ export default() => {
     useEffect(() =>{
         fetchPosts();
     },[]);
-    return<div />;
+
+    const renderedPosts = Object.values(posts).map((post) => {
+        return (
+            <div
+            className="card"
+            style={{width: '30%', marginBottom:'20px'}}
+            key= {post.id}>
+                <div
+                className="card-body">
+                    <h3>{post.title}</h3>
+                </div>
+            </div>
+        );
+    });
+
+    return <div className="d-flex flex-row flex-wrap justify-content-between">
+        {renderedPosts}
+    </div>;
 };
