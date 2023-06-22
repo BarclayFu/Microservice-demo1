25c23c6143b56503b59ecca5b0dd06ce3d4e1c95
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-06-22 00:15:50 -0400

    update query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,19 @@
 import React, {useEffect, useState} from "react";
-import axios from 'axios';
+// import axios from 'axios';
 
-export default({postId}) => {
+export default({comments}) => {
 
-    const [comments, setComments] = useState([]);
+    // const [comments, setComments] = useState([]);
 
-    const fetchData = async() => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
+    // const fetchData = async() => {
+    //     const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
         
-        setComments(res.data);
-    };
+    //     setComments(res.data);
+    // };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //     fetchData();
+    // }, []);
 
     const renderedComments = comments.map(comment => {
         return <li key = {comment.id}>{comment.content}</li>
